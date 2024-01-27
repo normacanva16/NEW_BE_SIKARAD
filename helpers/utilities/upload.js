a7910afe019fcee407232061247caabd2f5c1cb1
normarosita16 <norma.rosita.damayanthi.16@gmail.com>
2024-01-27 13:37:09 +0700

    fix

--- helpers/utilities/upload.js
+++ helpers/utilities/upload.js
@@ -37,7 +37,7 @@ const docFilter = (req, file, cb) => {
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null,path.join(__dirname, '../../uploads'));
+    cb(null,path.join(__dirname, '../../tmp'));
   },
   filename: (req, file, cb) => {
     console.log(file.originalname);
