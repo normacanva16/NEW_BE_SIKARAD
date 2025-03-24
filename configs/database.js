5bcb6786eca128a567c32a359cfc98bf9fe2b3d7
ujangaripin24 <ujangarifin06@gmail.com>
2025-03-24 22:20:55 +0700

    DATABASE HAS BEEN CONNECT TO SUPABASE

--- configs/database.js
+++ configs/database.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-    username: 'postgres.ceozmkohwvnkreqwqtqw', 
-    password: 'Norma161197120603',
+    username: 'postgres.etsnykktdiyhqarpaami', 
+    password: 'k4aNEOTGv4MHd8Q0',
     database: 'postgres',
     host: 'aws-0-ap-southeast-1.pooler.supabase.com',
     dialect: 'postgres',
     timezone: '+07:00',
     port: 6543,
     schema: 'public',
-    //migrationStorageTableSchema: "public",
-    //schema: process.env.DB_SCHEMA,
+    // migrationStorageTableSchema: "public",
+    // schema: process.env.DB_SCHEMA,
 };
